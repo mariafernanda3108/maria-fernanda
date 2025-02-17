033369869206e6ea76d47a96c79737345e7ff9ed
mariafernanda3108 <mariafernandasantos3108@gmail.com>
2025-02-17 19:51:06 +0000

    estrutura condicuinal

---
+++ 1tipos de dados/ 3tipagem_dinamica.py
@@ -0,0 +1,12 @@
+import os
+os.system("clear")
+
+# solicitando dados.
+nome = input("Digite seu nome: ")
+idade = int(input( "Digite sua idade: "))
+altura = float(input("Digite sua altura: "))
+
+# escreval("Nome: ", nome")
+print(f"Nome: (nome)")
+print(f"idade: (idade)")
+print(f"altura: (altura)")
