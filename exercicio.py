51e556332bd2bf91fe5c2042e030a2a36ab1c7c4
mariafernanda3108 <mariafernandasantos3108@gmail.com>
2025-02-24 19:56:14 +0000

    salvar

---
+++ exercicio.py
@@ -0,0 +1,13 @@
+import os 
+os.system("clear")
+
+login = maria_fernanda
+senha = maria123
+
+login = input("Digite seu login: ")
+senha = input("Digite sua senha: ")
+ 
+if login == login_cadastrado and senha == senha_cadastrada
+    print("Bem vindo")
+else 
+    print('login ou senha invalidos: ")
