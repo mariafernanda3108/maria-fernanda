a787550f147bcab1e20788ae01a2e4bd149e592e
mariafernanda3108 <mariafernandasantos3108@gmail.com>
2025-05-21 19:46:16 +0000

    salvar

---
+++ Atividade_nova2.py/explicacao.py
@@ -0,0 +1,7 @@
+<!-- TAG -->
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+        <meta charset= "UTF-8">
+        <meta name="viewport" content="widht=device-widht, initial-scal-1.0">
+        <title>DOCUMENT</title>
